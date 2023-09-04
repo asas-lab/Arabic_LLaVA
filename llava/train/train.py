5971c707248eb13a108b1f871943e96754766500
KhalidAlt <ekh@Protonmail.com>
2023-09-04 07:14:14 +0300

    integrate jais with train

--- llava/train/train.py
+++ llava/train/train.py
@@ -776,6 +776,14 @@ def train():
                 cache_dir=training_args.cache_dir,
                 **bnb_model_from_pretrained_args
             )
+        elif 'jais' in model_args.model_name_or_path:
+            config = transformers.AutoConfig.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
+            model = LlavajaisForCausalLM.from_pretrained(
+                model_args.model_name_or_path,
+                cache_dir=training_args.cache_dir,
+                **bnb_model_from_pretrained_args
+            )
+
         else:
             config = transformers.AutoConfig.from_pretrained(model_args.model_name_or_path, trust_remote_code=True)
             model = LlavahfForCausalLM.from_pretrained(
